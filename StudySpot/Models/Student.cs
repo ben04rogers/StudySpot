f100532f11ef1db48ac220db9f074b21fc9ab5d4
Ben Rogers <benrogers1299@outlook.com>
2020-09-16 22:25:47 +1000

    Started Student Model

---
+++ StudySpot/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StudySpot.Models
+{
+    public class Student
+    {
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string id { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+
+    }
+}
