41febcfb2c4f79d2ecc99c07da11fe6a27909969
William M <n10491694@qut.edu.au>
2020-09-18 19:43:15 +1000

    Colour changes to nav bar; changed last nav button to point to feed page; added feed page and viewmodel; added announcements model

---
+++ StudySpot/Models/Announcements.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudySpot.Models
+{
+    enum type
+    {
+        Important, Reminder
+    }
+
+    class Announcements
+    {
+        public string unit { get; set; }
+        public string date { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public string type { get; set; }
+    }
+}
---
+++ StudySpot/ViewModels/FeedViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using StudySpot.Models;
+using Xamarin.Forms;
+
+namespace StudySpot.ViewModels
+{
+    public class FeedViewModel : BaseViewModel
+    {
+        // Fields
+        private List<string> announcementText;
+        private int announcementTextIndex;
+
+        // Properties
+        public string AnnouncementText
+        {
+            get
+            {
+                // Get announcement at index (index incrementing with each Binding call)
+                announcementTextIndex++; // Initial = -1
+                string text = announcementText[announcementTextIndex];
+                return text;
+            }
+        }
+        public Command<string> GoToAnnouncements { get; }
+
+        // Constructor
+        public FeedViewModel()
+        {
+            // Initialise
+            announcementText = new List<string>();
+            announcementTextIndex = -1;
+            // Button click method with parameter
+            GoToAnnouncements = new Command<string>(onAnnouncementClick);
+
+            // Get announcement texts
+            announcementText.Add("28 Jul | Room Change (get from Model)");
+            announcementText.Add("10 Aug | Due Date (get from Model)");
+        }
+
+        // Methods
+        private async void onAnnouncementClick(string announcementType)
+        {
+            // Test
+            string newObj = announcementType;
+            // Navigate to announcements page view
+            await Shell.Current.GoToAsync("AnnouncementsPage");
+        }
+    }
+}
