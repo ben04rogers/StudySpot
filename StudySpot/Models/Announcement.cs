031e6978ea37504b2371c0c338fd066a7dc1589c
William M <n10491694@qut.edu.au>
2020-09-23 02:42:06 +1000

    Announcements viewmodel

---
+++ StudySpot/Models/Announcement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudySpot.Models
+{
+    enum type
+    {
+        Important, Reminder
+    }
+
+    public class Announcement
+    {
+        //public string Unit { get; set; }
+        public string Date { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        //public string Type { get; set; }
+    }
+}
---
+++ StudySpot/ViewModels/AnnouncementsViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using System.Collections.ObjectModel;
+using StudySpot.Models;
+using Xamarin.Forms;
+
+namespace StudySpot.ViewModels
+{
+    public class AnnouncementsViewModel : BaseViewModel
+    {
+        // Fields
+
+        // Properties
+        public ObservableCollection<Announcement> GetAnnouncements { get; private set; }
+
+        // Constructor
+        public AnnouncementsViewModel()
+        {
+            // Initialise
+            GetAnnouncements = new ObservableCollection<Announcement>();
+
+            GetData();
+        }
+
+        // Methods
+        void GetData()
+        {
+            // Get announcement text
+            GetAnnouncements.Add(new Announcement
+            {
+                Date = "21 Jul",
+                Title = "Annoucement 1",
+                Description = "Lorem Ipsum"
+            });
+            GetAnnouncements.Add(new Announcement
+            {
+                Date = "28 Jul",
+                Title = "Annoucement 2",
+                Description = "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum"
+            });
+        }
+    }
+}
