4a2126140b28b2aa0c5f89d787ab8e9bdee89f91
ben04rogers <47819009+ben04rogers@users.noreply.github.com>
2020-09-27 18:08:54 +1000

    Update

--- StudySpot.Android/Resources/Resource.designer.cs
+++ StudySpot.Android/Resources/Resource.designer.cs
@@ -8400,22 +8400,25 @@ namespace StudySpot.Droid
 			public const int notify_panel_notification_icon_bg = 2131165306;
 			
 			// aapt resource value: 0x7F07007B
-			public const int tab_about = 2131165307;
+			public const int settings = 2131165307;
 			
 			// aapt resource value: 0x7F07007C
-			public const int tab_feed = 2131165308;
+			public const int tab_about = 2131165308;
 			
 			// aapt resource value: 0x7F07007D
-			public const int tooltip_frame_dark = 2131165309;
+			public const int tab_feed = 2131165309;
 			
 			// aapt resource value: 0x7F07007E
-			public const int tooltip_frame_light = 2131165310;
+			public const int tooltip_frame_dark = 2131165310;
 			
 			// aapt resource value: 0x7F07007F
-			public const int xamarin_logo = 2131165311;
+			public const int tooltip_frame_light = 2131165311;
 			
 			// aapt resource value: 0x7F070080
-			public const int zoom = 2131165312;
+			public const int xamarin_logo = 2131165312;
+			
+			// aapt resource value: 0x7F070081
+			public const int zoom = 2131165313;
 			
 			static Drawable()
 			{
