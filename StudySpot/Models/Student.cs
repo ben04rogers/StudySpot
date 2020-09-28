9f9b9b315f808a95d9978f042201c24905449016
ben04rogers <47819009+ben04rogers@users.noreply.github.com>
2020-09-28 14:26:32 +1000

    Update Student.cs

--- StudySpot/Models/Student.cs
+++ StudySpot/Models/Student.cs
@@ -4,11 +4,11 @@ namespace StudySpot.Models
 {
     public class Student
     {
-        public string first_name { get; set; }
-        public string last_name { get; set; }
-        public string id { get; set; }
-        public string email { get; set; }
-        public string password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
 
     }
 }
