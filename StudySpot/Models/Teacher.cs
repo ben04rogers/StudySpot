9d5dfa517654f8e106f7e96eee9d7a9f6aeb6fef
ben04rogers <47819009+ben04rogers@users.noreply.github.com>
2020-09-28 14:24:47 +1000

    Started Teacher and Unit models

---
+++ StudySpot/Models/Teacher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudySpot.Models
+{
+    public class Teacher
+    {
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Email { get; set; }
+        public string Unit { get; set; }
+
+
+    }
+}
---
+++ StudySpot/Models/Unit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudySpot.Models
+{
+    public class Unit
+    {
+        public string Name { get; set; }
+        public string UnitCode { get; set; }
+        public string Description { get; set; }
+    }
+}
