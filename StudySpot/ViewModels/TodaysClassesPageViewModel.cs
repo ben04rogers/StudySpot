2bc9c7bfd37f89b404ddcdab8b971263f54b5f67
ben04rogers <47819009+ben04rogers@users.noreply.github.com>
2020-09-30 13:43:36 +1000

    Added Data to Todays Classes Page

--- StudySpot/ViewModels/TodaysClassesPageViewModel.cs
+++ StudySpot/ViewModels/TodaysClassesPageViewModel.cs
@@ -10,9 +10,46 @@ namespace StudySpot.ViewModels
 {
     public class TodaysClassesPageViewModel : BaseViewModel
     {
+        public ObservableCollection<TodaysClass> TodaysClasses { get; set; }
+
         public TodaysClassesPageViewModel()
         {
+            SetupData();
+
             Title = "Todays Classes";
+        }
+
+
+        // Dummy Data to test with
+        void SetupData()
+        {
+            TodaysClasses = new ObservableCollection<TodaysClass>()
+            {
+                new TodaysClass
+                {
+                    UnitCode = "CAB303",
+                    Time = "9:00",
+                    TimePeriod = "AM",
+                    LessonType = "Online Workshop",
+                    Platform = "Zoom ID: 937109249"
+                },
+                new TodaysClass
+                {
+                    UnitCode = "IAB330",
+                    Time = "11:00",
+                    TimePeriod = "AM",
+                    LessonType = "Online Tutorial",
+                    Platform = "Microsoft Teams"
+                },
+                new TodaysClass
+                {
+                    UnitCode = "IAB330",
+                    Time = "11:00",
+                    TimePeriod = "AM",
+                    LessonType = "Online Tutorial",
+                    Platform = "Microsoft Teams"
+                }
+            };
         }
     }
 }
