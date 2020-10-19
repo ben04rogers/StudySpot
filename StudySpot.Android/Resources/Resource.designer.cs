3094307d2e2d56af5f63ec8756b4a3a8c8f48a9d
Ben Rogers <47819009+ben04rogers@users.noreply.github.com>
2020-10-19 10:18:01 +1000

    Update settings images to match

--- StudySpot.Android/Resources/Resource.designer.cs
+++ StudySpot.Android/Resources/Resource.designer.cs
@@ -8421,25 +8421,34 @@ namespace StudySpot.Droid
 			public const int profile = 2131165313;
 			
 			// aapt resource value: 0x7F070082
-			public const int settings = 2131165314;
+			public const int profile_black = 2131165314;
 			
 			// aapt resource value: 0x7F070083
-			public const int tab_about = 2131165315;
+			public const int settings = 2131165315;
 			
 			// aapt resource value: 0x7F070084
-			public const int tab_feed = 2131165316;
+			public const int settings_black = 2131165316;
 			
 			// aapt resource value: 0x7F070085
-			public const int tooltip_frame_dark = 2131165317;
+			public const int settings_white = 2131165317;
 			
 			// aapt resource value: 0x7F070086
-			public const int tooltip_frame_light = 2131165318;
+			public const int tab_about = 2131165318;
 			
 			// aapt resource value: 0x7F070087
-			public const int xamarin_logo = 2131165319;
+			public const int tab_feed = 2131165319;
 			
 			// aapt resource value: 0x7F070088
-			public const int zoom = 2131165320;
+			public const int tooltip_frame_dark = 2131165320;
+			
+			// aapt resource value: 0x7F070089
+			public const int tooltip_frame_light = 2131165321;
+			
+			// aapt resource value: 0x7F07008A
+			public const int xamarin_logo = 2131165322;
+			
+			// aapt resource value: 0x7F07008B
+			public const int zoom = 2131165323;
 			
 			static Drawable()
 			{
