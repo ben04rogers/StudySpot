5a472b1b649b61ee21fe2a470a7e29a2b81d35e1
Ben Rogers <47819009+ben04rogers@users.noreply.github.com>
2020-10-19 11:51:16 +1000

    Splash Screen

--- StudySpot.Android/MainActivity.cs
+++ StudySpot.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace StudySpot.Droid
 {
-    [Activity(Label = "StudySpot", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    [Activity(Label = "StudySpot", Icon = "@mipmap/icon", Theme = "@style/MainTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
---
+++ StudySpot.Android/SplashScreen.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Support.V7.App;
+
+
+namespace StudySpot.Droid
+{
+    [Activity(Theme = "@style/SplashScreen", MainLauncher = true, NoHistory = true)]
+    public class SplashScreen : AppCompatActivity
+    {
+        public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
+        {
+            base.OnCreate(savedInstanceState, persistentState);
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            // Launch the main activity
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+
+        public override void OnBackPressed() { }
+    }
+}
