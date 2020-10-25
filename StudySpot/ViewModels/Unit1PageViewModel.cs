353c69dfe309ccfb1231a50358698f3a5befd55a
Ben Rogers <benrogers1299@outlook.com>
2020-10-25 17:47:06 +1000

    Revert "Revert "name button""
    This reverts commit 298e98be66ff7a5cef942e92e3dbe4287ae138e5.

--- StudySpot/ViewModels/Unit1PageViewModel.cs
+++ StudySpot/ViewModels/Unit1PageViewModel.cs
@@ -10,18 +10,34 @@ namespace StudySpot.ViewModels
     public class Unit1PageViewModel : BaseViewModel
     {
 
-        public Command Staffdetail { get; }
+        // button command
+        public Command Staff1detail { get; }
+        public Command Staff2detail { get; }
 
+        public Command Staff3detail { get; }
 
         public Unit1PageViewModel()
         {
-            
-            // Staff Details Button
-            //Staffdetail = new Command(DisplayStaffDetails);
+            Staff1detail = new Command(GoToStaff1DetailPage);
+            Staff2detail = new Command(GoToStaff2DetailPage);
+            Staff3detail = new Command(GoToStaff3DetailPage);
+
         }
 
-        
+        async void GoToStaff1DetailPage()
+        {
+            await Shell.Current.GoToAsync("Staff1Page");
+        }
 
+        async void GoToStaff2DetailPage()
+        {
+            await Shell.Current.GoToAsync("Staff2Page");
+        }
+
+        async void GoToStaff3DetailPage()
+        {
+            await Shell.Current.GoToAsync("Staff3Page");
+        }
 
 
     }
